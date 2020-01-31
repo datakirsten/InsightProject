2da0eaae05a65ebb0636cb88482c8fe06b1d9a4c
datakirsten <kirstenmirja@gmail.com>
2020-01-31 13:56:14 -0500

    cleaned and commented the existing code

--- projectname/projectname/getsynonyms.py
+++ projectname/projectname/getsynonyms.py
@@ -1,14 +1,13 @@
 # Import relevant tools for NLP and load the english language library
 import spacy
+#next step commented step is only necessary when loading of the english language library fails due to weird behaviour of anaconda
 #from spacy.cli import download
 #download('en')
 nlp = spacy.load('en_core_web_sm')
 #import numpy as np
 import pandas as pd
-
 import nltk
 nltk.download('wordnet')
-import requests
 from gensim.summarization import keywords
 
 #from sklearn.pipeline import Pipeline
@@ -17,17 +16,16 @@ from gensim.summarization import keywords
 from bs4 import BeautifulSoup
 import requests
 
-# Location of website and getting the content with BeautifulSoup
-url = "https://towardsdatascience.com/late-payment-practices-is-this-the-ticking-time-bomb-of-the-uk-economy-5b958e4dd109"
+# Example of an url input website
+##url = "https://towardsdatascience.com/can-analysts-and-statisticians-get-along-5c9a65c8d056"
 
-
+###function to scrape text from a medium article
 def getarticle(currenturl):
     webpage = requests.get(currenturl)
     content_page = webpage.content
     soup = BeautifulSoup(content_page, 'html.parser')
     title = []
     paragraphtext = []
-
     # get article title
     try:
         abody = soup.find('title')
@@ -35,19 +33,24 @@ def getarticle(currenturl):
     except:
         title = 'Anonymous'
     ## get main article text
-    # articlebody = soup.find(class_='article article-first-row').find_all('p')
     articlebody = soup.find(class_='meteredContent').find_all('p')
-
     # put main body of text into a list with the text structured paragraph by paragraph
     for paragraph in articlebody[:]:
         # get the text only
         text = paragraph.get_text()
         paragraphtext.append(text)
+    ###try to combine short paragraphs into one
+   # newparagraphtext = [[paragraphtext[0]]]
+   #  for paragraph in paragraphtext[1:]:
+   #      if len(paragraph.split())<20:
+   #          newparagraphtext[-1][0]=newparagraphtext[-1][0]+ paragraph
+   #      else:
+   #          newparagraphtext.append([paragraph])
 
-    return title,paragraphtext
+    return title,paragraphtext #,newparagraphtext
 
-b=getarticle(url)
-#print(b[1])
+###function that reads in the SUBTLEXus corpus and finds frequency of all input tokens in it
+###returns the most infrequent word
 def getfrequencySUBTLEX(input_paragraph):
     # read in subtlexus corpus, large corpus with frequency information
     article_lemma = nlp(input_paragraph)
@@ -75,22 +78,30 @@ def getfrequencySUBTLEX(input_paragraph):
  #   a=getfrequencySUBTLEX(str(paragraph))
   #  print(a)
 
+###use gensim's text summarization to find keywords in a paragraph, returns the keyword
+###lemmatization part does not work right now!
 def findkeywords(input_paragraph):
-    keyword=keywords(input_paragraph, words=1,scores=False, lemmatize=False, pos_filter=('NN', 'VB'))
+    keyword=keywords(input_paragraph, words=1,scores=False, lemmatize=True, pos_filter=('NN', 'NNS'))
     return keyword
 
+###gensim's entropy based keyword finder
+#def findkeywords_mz(input_paragraph):
+#    keyword=keywords(input_paragraph, words=1,scores=False, lemmatize=False, pos_filter=('NN', 'NNS'))
+#    return keyword
+
+###get infrequent words using TDIDF (still needs to be moved here from jupyter notebook
+#def getfrequencyTFIDF(input_paragraph):
+ #   vectorizer = TfidfVectorizer()
+ #   X_articles_tfidf = vectorizer.fit_transform(df[input_paragraph])  # remember to use the original X_train set
+#    X_articles_tfidf.shape
 
 
-def getfrequencyTFIDF(input_paragraph):
-    vectorizer = TfidfVectorizer()
-    X_articles_tfidf = vectorizer.fit_transform(df[input_paragraph])  # remember to use the original X_train set
-    X_articles_tfidf.shape
-
-####fix this!!!
+###get definitions (and synonyms) using wordnet
 def getsynforinfreq(word):
     from nltk.corpus import wordnet as wn  # Import wordnet from the NLTK
     syn = []
     for synset in wn.synsets(str(word)):
+        ##get the synonyms
         #for lemma in synset.lemmas():
             #if str(lemma.name()) != word:
              #   syn.append(lemma.name())  # add the synonyms
--- projectname/server.py
+++ projectname/server.py
@@ -1,6 +1,7 @@
+#import relevant tools to create a flask app
 from flask import Flask, render_template, request
 from projectname import getsynonyms
-import pandas as pd
+
 # Create the application object
 app = Flask(__name__)
 
@@ -14,64 +15,65 @@ def recommendation_output():
 	# Pull input
 	some_input = request.args.get('user_input')
 
-	# Case if empty
+	# Case if input empty
+	# Use the example from the website shown
 	if some_input == "":
-		return render_template("index.html",
-			my_input=some_input,
-			my_title="Please enter a medium website")
-	else:
-		some_title, some_maintext = getsynonyms.getarticle(some_input)
-		paragraph=[]
-		word1=[]
-		def1=[]
-		keyword=[]
-		parabefore=[]
-		parakey=[]
-		paraafter=[]
-		for paragraphs in some_maintext:
-			paragraph.append(paragraphs)
-			words=getsynonyms.getfrequencySUBTLEX(str(paragraphs))
-			word1.append(words)
-			def1.append(getsynonyms.getsynforinfreq(words))
-			currentkeyword=getsynonyms.findkeywords(paragraphs)
-			keyword.append(currentkeyword)
-			if not currentkeyword:
-				parabefore.append(paragraphs)
-				parakey.append([])
-				paraafter.append([])
-			else:
-				parapartition=paragraphs.partition(currentkeyword)
-				parabefore.append(parapartition[0])
-				parakey.append(parapartition[1])
-				paraafter.append(parapartition[2])
-		return render_template("index.html",
-			my_input=some_input,
-			my_title=some_title,
-			my_maintextbefore=parabefore[0],
-			my_maintextkey=parakey[0],
-			my_maintextafter=paraafter[0],
-			my_first_word=word1[0],
-			my_first_definition=def1[0],
-			my_first_keyword=keyword[0],
-			my_maintextbefore2=parabefore[1],
-			my_maintextkey2=parakey[1],
-			my_maintextafter2=paraafter[1],
-			my_second_word=word1[1],
-			my_second_definition=def1[1],
-			my_second_keyword=keyword[1],
-			my_maintextbefore3=parabefore[2],
-			my_maintextkey3=parakey[2],
-			my_maintextafter3=paraafter[2],
-			my_third_word=word1[2],
-			my_third_definition=def1[2],
-			my_third_keyword=keyword[2],
-			my_maintextbefore4=parabefore[3],
-			my_maintextkey4=parakey[3],
-			my_maintextafter4=paraafter[3],
-			my_fourth_word=word1[3],
-			my_fourth_definition=def1[3],
-			my_fourth_keyword=keyword[3],
-			my_form_result="NotEmpty")
+		some_input="https://towardsdatascience.com/late-payment-practices-is-this-the-ticking-time-bomb-of-the-uk-economy-5b958e4dd109"
+
+	###use the functions in the getsynonyms.py file to scrape the website text and provide the TextHelper tool output to the flas app
+	some_title, some_maintext = getsynonyms.getarticle(some_input)
+	paragraph=[]
+	word1=[]
+	def1=[]
+	keyword=[]
+	parabefore=[]
+	parakey=[]
+	paraafter=[]
+	for paragraphs in some_maintext:
+		paragraph.append(paragraphs)
+		words=getsynonyms.getfrequencySUBTLEX(str(paragraphs))
+		word1.append(words)
+		def1.append(getsynonyms.getsynforinfreq(words))
+		currentkeyword=getsynonyms.findkeywords(paragraphs)
+		keyword.append(currentkeyword)
+		if not currentkeyword:
+			parabefore.append(paragraphs)
+			parakey.append([])
+			paraafter.append([])
+		else:
+			parapartition=paragraphs.partition(currentkeyword)
+			parabefore.append(parapartition[0])
+			parakey.append(parapartition[1])
+			paraafter.append(parapartition[2])
+	##todo make the output more dynamic so it can adapt to number of paragraphs & generate the table accordingly
+	return render_template("index.html",
+		my_input=some_input,
+		my_title=some_title,
+		my_maintextbefore=parabefore[0],
+		my_maintextkey=parakey[0],
+		my_maintextafter=paraafter[0],
+		my_first_word=word1[0],
+		my_first_definition=def1[0],
+		my_first_keyword=keyword[0],
+		my_maintextbefore2=parabefore[1],
+		my_maintextkey2=parakey[1],
+		my_maintextafter2=paraafter[1],
+		my_second_word=word1[1],
+		my_second_definition=def1[1],
+		my_second_keyword=keyword[1],
+		my_maintextbefore3=parabefore[2],
+		my_maintextkey3=parakey[2],
+		my_maintextafter3=paraafter[2],
+		my_third_word=word1[2],
+		my_third_definition=def1[2],
+		my_third_keyword=keyword[2],
+		my_maintextbefore4=parabefore[3],
+		my_maintextkey4=parakey[3],
+		my_maintextafter4=paraafter[3],
+		my_fourth_word=word1[3],
+		my_fourth_definition=def1[3],
+		my_fourth_keyword=keyword[3],
+		my_form_result="NotEmpty")
 
 
 # start the server with the 'run()' method
